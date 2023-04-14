887b4a31d2fef2744dfeffdfb2efbbb6ff9e7034
Daiana <dauipoma@gmail.com>
2023-04-14 22:05:06 +0200

    fetch API starships

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from 'react'
+import StarshipCard from './components/StarshipCard'
+import { Header } from './components/Header'
+
+const API_URL = 'https://swapi.dev/api/starships/'
+
 function App() {
-  return <main className='App'></main>
+  const [starships, setStarships] = useState([])
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(data => {
+        const { results } = data
+        setStarships(prev => [...prev, ...results])
+      })
+  }, [])
+
+  return (
+    <>
+      <Header />
+      <main>
+        <section className='flex flex-col gap-2'>
+          {starships.map((s, i) => (
+            <StarshipCard
+              key={i}
+              name={s.name}
+              model={s.model}
+            />
+          ))}
+        </section>
+      </main>
+    </>
+  )
 }
 
 export default App
