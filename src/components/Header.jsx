2f01e3ccdac28a80cad0a724915fc60406ba582e
Daiana <dauipoma@gmail.com>
2023-04-17 18:34:37 +0200

    Refactor components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,20 @@
 import { NavLink } from 'react-router-dom'
 import logo from '../assets/logo.webp'
+import { FaRegUser } from 'react-icons/fa'
+import kids from '../assets/kids.svg'
 
 export function Header() {
   return (
     <header className='py-3'>
-      <img className='w-52 m-auto' src={logo} alt='Star Wars logo.' />
+      <div className='w-5/6 mx-auto flex flex-wrap justify-between items-center'>
+        <a href='https://www.starwarskids.com/'>
+          <img src={kids} alt='Star Wars Kids logo.' />
+        </a>
+        <img className='max-h-20' src={logo} alt='Star Wars logo.' />
+        <NavLink to={'/login'} className='flex items-center gap-2'>
+          <FaRegUser /> SIGN IN
+        </NavLink>
+      </div>
       <nav className='flex justify-center gap-3 border-y border-slate-600 border-solid text-gray-500 px-1 mt-2'>
         <NavLink
           className={({ isActive }) => (isActive ? 'border-b' : undefined)}
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,7 +6,7 @@ export default function Layout() {
   return (
     <>
       <Header />
-      <main className='text-center'>
+      <main className='container mx-auto'>
         <Outlet />
       </main>
       <Footer />
