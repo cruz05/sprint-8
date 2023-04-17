95423eda17391e9c4eb62ebca3e3f7a0168c75d0
Daiana <dauipoma@gmail.com>
2023-04-17 18:32:04 +0200

    Add more routes

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from '../components/Layout'
 import StarshipDetails from '../pages/StarshipDetails'
+import { Form } from '../components/Form'
 import StarshipList from '../pages/StarshipList'
 import Home from '../pages/Home'
+import { NotFound } from '../pages/NotFound'
 import { StarshipsContextProvider } from '../context/StarshipsContext'
 
 export function Router() {
@@ -16,6 +18,9 @@ export function Router() {
               <Route index element={<StarshipList />} />
               <Route path=':id' element={<StarshipDetails />} />
             </Route>
+            <Route path='/register' element={<Form />}></Route>
+            <Route path='/login' element={<Form />}></Route>
+            <Route path='/*' element={<NotFound />}></Route>
           </Route>
         </Routes>
       </StarshipsContextProvider>
